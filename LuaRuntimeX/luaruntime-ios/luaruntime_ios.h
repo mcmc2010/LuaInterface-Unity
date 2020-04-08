157e5ae3618ee4396b90bcca68f4ae6be3e9674b
FlyApple <magicskyapple@gmail.com>
2020-04-09 02:26:39 +0800

    20200409-1

---
+++ LuaRuntimeX/luaruntime-ios/luaruntime_ios.h
@@ -0,0 +1,13 @@
+//
+//  luaruntime_ios.h
+//  luaruntime-ios
+//
+//  Created by enos sariel on 2020/4/9.
+//  Copyright © 2020 enos sariel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface luaruntime_ios : NSObject
+
+@end
---
+++ LuaRuntimeX/luaruntime-ios/luaruntime_ios.m
@@ -0,0 +1,13 @@
+//
+//  luaruntime_ios.m
+//  luaruntime-ios
+//
+//  Created by enos sariel on 2020/4/9.
+//  Copyright © 2020 enos sariel. All rights reserved.
+//
+
+#import "luaruntime_ios.h"
+
+@implementation luaruntime_ios
+
+@end
